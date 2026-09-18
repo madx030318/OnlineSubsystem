9c70cbeaf4312c0db0ab8ff455bb54103e86bcfa
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-19 00:09:00 +0500

    Create ServerData.cpp

---
+++ ServerData.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+GameServer::GameServer() {
+  ServerSocket = 0;
+  Running = false;
+
+void GameServer::Start() {
+
+  Running = true;
+  ServerSocket;
+  
+};
+
+void GameServer::Stop() {
+
+  Running = false;
+  ServerSocket = 0;
+};
+void GameServer::AcceptClient() {
+
+};
+
+void GameServer::HandleClient(){
+
+}
+
+}
+
+
+}
+
